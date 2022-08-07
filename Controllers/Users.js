a82707e316ee2309cf307ea920b6a0384a2968e8
Kauavitorio <kauavitorioof@gmail.com>
2022-08-06 21:52:17 -0300

    Migrate to Sql Server (#2)

--- Controllers/Users.js
+++ Controllers/Users.js
@@ -1,24 +1,12 @@
-const mysql = require('../mysql')
+const mSqlHelper = require('../SqlHelper')
 
 //  Method to confirm User Email
 exports.SignUp = async (req, res, next) => {
     try {
-        var account_id = req.body.account_id_cry
-        var verify_id = req.body.verify_id
-
-        var result_select = await mysql.execute('SELECT * FROM tbl_account WHERE account_id = ?', EncryptDep.Decrypt(account_id))
-        if(result_select.length > 0){
-            var get_verify_id = EncryptDep.Decrypt(result_select[0].verify_id)
-            if(get_verify_id == EncryptDep.Decrypt(verify_id)){
-                const queryUpdate = `UPDATE tbl_account set verify_id = "Confirmed", verify = 1 WHERE account_id = ?;`
-                    await mysql.execute(queryUpdate, EncryptDep.Decrypt(account_id));
-                    return res.status(200).send({message: EncryptDep.Encrypto('Confirmed email')})
-            }else
-                return res.status(203).send({ message: EncryptDep.Encrypto('Invalid code')})
-        }else
-            return res.status(404).send({ message: EncryptDep.Encrypto('User not found')})
+        var result_select = await mSqlHelper.Execute('SELECT * FROM ADSP010')
+        return res.status(200).send(result_select)
     } catch (error) {
-        ServerDetails.RegisterServerError('Validate User Email', error.toString())
-        return res.status(500).send({ message: error.toString()})
+        console.log(error)
+        return res.status(500).send(error)
     }
 }
